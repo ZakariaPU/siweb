2dfa742346b8cc1c9c118a0f82ffe5d823f1d796
ZakariaPU <bangzakaria869@gmail.com>
2023-03-06 05:22:08 +0700

    Boostrap

---
+++ app/Controllers/Admin/Master.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\controllers\Admin;
+
+use App\Controllers\BaseController;
+
+class Master extends BaseController
+{
+    public function index()
+    {
+        echo "Ini Adalah Halaman Master";
+    }
+}
